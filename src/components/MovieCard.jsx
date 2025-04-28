9873984b95d87fbfb4123d9b9900ebf34152dc56
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-04-28 12:43:16 +0200

    Refactor MovieCard component: update column classes for responsive design and enhance image styling

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,10 +5,10 @@ export default function MovieCard({ movie }) {
     const { id, image, title, director } = movie
 
     return (
-        <div className="col">
+        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
             <div className="card h-100">
                 <Link to={`/movies/${id}`}>
-                    <img src={`http://localhost:3000/${image}`} alt={title} className="card-img-top" />
+                    <img src={`http://localhost:3000/${image}`} alt={title} className="card-img-top img-fluid" />
                 </Link>
 
                 <div className="card-body">
